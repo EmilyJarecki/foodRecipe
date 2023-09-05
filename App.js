6af9935ae29690f3b26b8a9681f30f030df75789
EmilyJarecki <jareckiemily@yahoo.com>
2023-09-04 19:59:22 -0500

    tailwind installed and working

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import tw from "twrnc"
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-white text-3xl font-bold">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // you can change the context of the content
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
